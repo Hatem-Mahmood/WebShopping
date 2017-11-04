9af228cf5c929e834870e9d75e75be5f297a12e6
Hatem-Mahmood <33034067+Hatem-Mahmood@users.noreply.github.com>
2017-11-05 00:10:12 +0100

    Add firebase to project
    Firebase Realtime Database
    Store and sync data with  NoSQL cloud database. Data is synced across all clients in realtime, and remains available when your app goes offline.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCmecXK8Q3oYUGvF2u9HKvwl-sH0qQfeMQ",
+    authDomain: "oshop-dec23.firebaseapp.com",
+    databaseURL: "https://oshop-dec23.firebaseio.com",
+    projectId: "oshop-dec23",
+    storageBucket: "oshop-dec23.appspot.com",
+    messagingSenderId: "362428299122"    
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCmecXK8Q3oYUGvF2u9HKvwl-sH0qQfeMQ",
+    authDomain: "oshop-dec23.firebaseapp.com",
+    databaseURL: "https://oshop-dec23.firebaseio.com",
+    projectId: "oshop-dec23",
+    storageBucket: "oshop-dec23.appspot.com",
+    messagingSenderId: "362428299122"    
+  }
 };
